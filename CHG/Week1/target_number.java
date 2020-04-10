68ce66f0cd842f3f49e07d9aafc93387136d38b7
hgchoii <nicekatak@naver.com>
2020-04-10 23:56:09 +0900

    Create target_number.java

---
+++ CHG/Week1/target_number.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int count= 0;
+    
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        DFS(numbers, target, 0, 0);
+		answer = count;
+        return answer;
+    }
+    
+    public void DFS(int[] numbers, int target, int n, int sum) {
+		if(n == numbers.length) {
+			if(sum == target)
+				count++;
+			return;
+		}
+		
+		DFS(numbers, target, n+1, sum + numbers[n]);
+		DFS(numbers, target, n+1, sum - numbers[n]);
+	}
+}
