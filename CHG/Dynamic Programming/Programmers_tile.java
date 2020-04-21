8795a8cdb888f6c72af2cd4cce3ced2f19fa5863
hgchoii <nicekatak@naver.com>
2020-04-21 17:02:35 +0900

    Create Programmers_tile.java

---
+++ CHG/Dynamic Programming/Programmers_tile.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+class Solution {
+    public long solution(int N) {
+        long answer = 0;
+		
+		long[] sum = new long[81];
+		sum[1] = 4;
+		sum[2] = 6;
+		
+		for(int i=3; i<=N; i++) {
+			sum[i] = sum[i-1] + sum[i-2];
+		}
+		
+		answer = sum[N];
+		
+		return answer;
+    }
+}
