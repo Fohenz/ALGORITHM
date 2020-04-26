e384d9420604665d3212126e01fbb05abe176609
LKM <lkm1153@naver.com>
2020-04-27 02:30:14 +0900

    Create 도둑질.py
    dp 2개 만드는거 신박하넹...

---
+++ LKM/DP/도둑질.py
@@ -0,0 +1,11 @@
+def solution(money):
+    answer = 0
+    dp = [0 for _ in money]
+    dp2 = [0 for _ in money]
+    dp[0], dp[1], dp2[0], dp2[1] = money[0], money[0], 0, money[1] 
+    for i in range(2,len(money)-1):
+        dp[i] = max(dp[i-2] + money[i], dp[i-1])
+    for i in range(2,len(money)):
+        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1])
+    answer = max(dp[len(money)-2], dp2[len(money)-1])
+    return answer
