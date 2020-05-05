e8824834c27577953f4f1561bbec7f9a72d7238b
AerinLee <parrot0707@gmail.com>
2020-05-05 14:33:12 +0900

    Create 2016년.py

---
+++ ERIN/프로그래머스_연습문제/2016년.py
@@ -0,0 +1,7 @@
+def solution(a, b):
+    answer = ''
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED']
+    val = (sum(months[:a-1])+b)%7
+    answer = days[val]
+    return answer
